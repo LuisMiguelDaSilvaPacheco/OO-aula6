8f51c187af87881cea13a0945c4b4f5969dce926
Luís Miguel <lm.luismiguel10@gmail.com>
2026-05-20 15:18:25 -0300

    Initialize passagensVendidas and improve selling logic
    Refactor Veiculo class to initialize passagensVendidas and update passage selling logic.

--- atividade3-1.php
+++ atividade3-1.php
@@ -2,11 +2,14 @@
 
 class Veiculo{
     private int $capacidade;
-    private  int $passagensVendidas;
+    private int $passagensVendidas;
 
     public function __construct( int $capacidade)
     {
         $this->setCapacidade($capacidade);
+        $this->setPassagensVendidas(0);
+
+        return $this->capacidade;
     }
 
     public function getCapacidade()
@@ -33,13 +36,31 @@ class Veiculo{
         return $this;
     }
 
-    
+    public function venderPassagem(int $passagens)
+    {
 
-
+        if ($this->capacidade >= $passagens) {
+            $this->capacidade -= $passagens;
+            return true;
+        }
+        return false;
+    }
 }
 
-$veiculo = new Veiculo(200);
+$veiculo = new Veiculo(readline("Qual a capacidade inicial do veiculo? "));
+
 do {
-    $veiculo->setPassagensVendidas(readline('Quantas passagens você quer comprar? '));
-    $veiculo->venderPassagem();
-} while ($veiculo->getPassagensVendidas() > 0);
+
+    $passagens = 'Quantas passagens você quer comprar? ';
+
+    if ($veiculo->venderPassagem(readline($passagens))) {
+        print "Passagens compradas com sucesso!\n\n";
+    } else {
+        print "A quantidade de passagens que você deseja comprar excede a disponivel (". $veiculo->getCapacidade(). "), tente novamente.\n\n";
+    }
+    
+} while ($passagens > 0 && $veiculo->getCapacidade() > 0);
+
+print "Finalizando...";
+sleep(5);
+system("clear");
